fed956bfaa9860350f36a36f21362f354a50cada
Robert Snyder <r.snyder49@gmail.com>
2018-08-27 23:45:02 +0000

    automatically backed up by learn

--- lib/email_parser.rb
+++ lib/email_parser.rb
@@ -12,8 +12,8 @@ class EmailParser
   
   def parse 
     parsed_emails = []
-    email_str = emails.split
-    email_array = emails.split(/[,\s]/)
+    email_str = emails.join
+    email_array = email_str.split(/[,\s]/)
     email_array.each do |email|
       parsed_emails << email unless parsed_emails.include?(email)
     end 
